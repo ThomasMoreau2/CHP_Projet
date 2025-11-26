0ecec33d25569ab6169815d05b60cd7997a0ec44
Justin SIMON <justin.simon@bordeaux-inp.fr>
2025-11-26 15:01:25 +0100

    allgather

--- main.cpp
+++ main.cpp
@@ -57,7 +57,8 @@ int main(int argc, char *argv[])
     t = 0;
 
 
-    for (int i=0; i<N_loc; i++){U0[i]=0;}
+    for (int i=0; i<N_loc; i++){U0[i]=0;
+    }
 
     vector<double> U_g(Ny), U_d(Ny);
 
@@ -65,6 +66,7 @@ int main(int argc, char *argv[])
     {      
         int left = rank-1;
         int right = rank+1;
+        printf("Proc %d: itération %d\n", rank, i);
 
         if (rank==0){left = MPI_PROC_NULL;}
         if (rank==nproc-1){right = MPI_PROC_NULL;}
@@ -73,22 +75,33 @@ int main(int argc, char *argv[])
         MPI_Sendrecv(&U0[N_loc-r*Ny], Ny, MPI_DOUBLE, right, 0,
         &U_g[0], Ny, MPI_DOUBLE, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         
+        
+        
         // Envoie à gauche, reçoit de droite
         MPI_Sendrecv(&U0[(r-1)*Ny], Ny, MPI_DOUBLE, left,  1, 
         &U_d[0], Ny, MPI_DOUBLE, right, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+        
         t += dt;
         b = make_b(U0, N_loc/Ny, Ny, dx, dy, Lx, Ly, D, dt, t, cas, rank, nproc, U_g, U_d);
+        for (int i = 0; i < N_loc; i++)
+        {
+            printf("Proc %d: b[%d] = %f\n", rank, i, b[i]);
+        }
+        
         U = gradient_conjugue(N_loc/Ny, Ny, D, dx, dy, dt, b, 0.0001, 1000);
-
+        for (int i = 0; i < N_loc; i++)
+        {
+            printf("Proc %d: U[%d] = %f\n", rank, i, U[i]);
+        }
         U0=U;
     }
-
-
+    
+    
+    
     ofstream fichier("affichage.dat");
     for (int i=0; i<N; i++)
     {
-        fichier << (i/Ny+1)*dx << " " << (i%Ny+1)*dy << " " << U[i] << endl;
+        fichier << (i/Ny+1)*dx << " " << (i%Ny+1)*dy << " " << U_final[i] << endl;
     }
     fichier.close();
 
